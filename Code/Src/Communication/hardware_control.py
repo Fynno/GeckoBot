1fc58a0ed8fa5d88d3fa398e9ebec6609d298813
bianca <bianca@bianca.amp>
2018-08-22 14:00:32 +0200

    awd

--- Code/Src/Communication/hardware_control.py
+++ Code/Src/Communication/hardware_control.py
@@ -243,7 +243,7 @@ class HUIThread(threading.Thread):
 	    difangle = IMUcalc.calc_angle(startvec, endvec)
             difangle2 = math.acos(safety)*57.2958
 		
-            if abs(difangle) > (5):
+            if abs(difangle) > (10):
                 self.rootLogger.info('Unfixed foot identified')
 		self.ptrn_idx -=2
             else:
